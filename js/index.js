88d926d1f62211d64f5f7a127640838634aa856b
getmanovanton <getmanov.11@mail.ru>
2019-09-09 09:09:50 +0300

    commit13

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ var left = 0;
 function sliderLeft() {
     var polosa = document.getElementById('polosa');
     var land = document.getElementById('landth');
-    left = left -248;
+    left = left -281;
     landt = landt +49;
     if (landt > 196){
         landt = 0
     }
-    if (left < -992) {
+    if (left < -1124) {
         left = 0;
     }
     land.style.left = landt+'px';
@@ -21,12 +21,12 @@ function sliderRight(){
     var land = document.getElementById('landth');
     var polosa = document.getElementById('polosa');
     landt = landt -49;
-    left = left +248;
+    left = left +281;
     if (landt < 0){
         landt = 196;
     }
     if (left > 0) {
-        left = -992;
+        left = -1124;
     }
     land.style.left = landt+'px';
     polosa.style.left = left+'px';
